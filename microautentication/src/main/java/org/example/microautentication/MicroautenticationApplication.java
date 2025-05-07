6260267e2fb9d3ae94c05faca39649a3fd6019fb
efpcode <efpjobhunt@gmail.com>
2025-05-07 13:19:57 +0200

    Adds microserivce authentication layout

---
+++ microautentication/src/main/java/org/example/microautentication/MicroautenticationApplication.java
@@ -0,0 +1,13 @@
+package org.example.microautentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroautenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroautenticationApplication.class, args);
+    }
+
+}
